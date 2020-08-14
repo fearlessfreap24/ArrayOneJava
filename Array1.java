4d57c8edca98a45e41a5be1df59fa0c6856177be
fearlessfreap24 <dylanaperez@yahoo.com>
2020-08-14 18:20:59 -0500

    completed commonEnd, sum3, and rotateLeft3

--- Array1.java
+++ Array1.java
@@ -29,8 +29,41 @@ public class Array1 {
 //			System.out.printf("nums = %s : %b\n", Arrays.toString(num2), sameFirstLast(num2));
 //		}
 		
-		System.out.printf("makePi() = %s", Arrays.toString(makePi()));
+//		System.out.printf("makePi() = %s", Arrays.toString(makePi()));
 		
+//		int[][][] nums3 = { { {1, 2, 3}, {7, 3} }, 
+//				{ {1, 2, 3}, {7, 3, 2} }, 
+//				{ {1, 2, 3}, {1, 3} },
+//				{ {1, 2, 3}, {1} },
+//				{ {1, 2, 3}, {2} } };
+//		for ( int[][] num3 : nums3 ) {
+//			System.out.printf("a = %s, b = %s : %b\n", Arrays.toString(num3[0]), Arrays.toString(num3[1]), commonEnd(num3[0], num3[1]));
+//		}
+		
+//		int[][] nums4 = { {1, 2, 3}, {5, 11, 2}, {7, 0, 0} };
+//		for ( int[] num4 : nums4 ) {
+//			System.out.printf("nums = %s : %d\n", Arrays.toString(num4), sum3(num4) );
+//		}
+		
+		int[][] nums5 = { {1, 2, 3}, {5, 11, 9}, {7, 0, 0} };
+		for ( int[] num5 : nums5 ) {
+			System.out.printf("nums = %s : %s\n", Arrays.toString(num5), Arrays.toString(rotateLeft3(num5)));
+		}
+		
+	}
+
+	private int[] rotateLeft3(int[] nums) {
+
+//		Given an array of ints length 3, return an array 
+//		with the elements "rotated left" so {1, 2, 3} yields 
+//		{2, 3, 1}.
+//
+//		rotateLeft3([1, 2, 3]) → [2, 3, 1]
+//		rotateLeft3([5, 11, 9]) → [11, 9, 5]
+//		rotateLeft3([7, 0, 0]) → [0, 0, 7]
+		
+		int[] rotateleft3 = { nums[1], nums[2], nums[0] };
+		return rotateleft3;
 	}
 
 	private boolean firstLast6(int[] nums) {
@@ -68,6 +101,32 @@ public class Array1 {
 //		makePi() → [3, 1, 4]
 		int[] makepi = {3, 1, 4}; 
 		return makepi;
+	}
+
+	private boolean commonEnd(int[] a, int[] b) {
+		
+//		Given 2 arrays of ints, a and b, return true if they 
+//		have the same first element or they have the same last 
+//		element. Both arrays will be length 1 or more.
+//
+//		commonEnd([1, 2, 3], [7, 3]) → true
+//		commonEnd([1, 2, 3], [7, 3, 2]) → false
+//		commonEnd([1, 2, 3], [1, 3]) → true
+		
+		return (a[a.length - 1] == b[b.length - 1])
+				|| (a[0] == b[0]);
+	}
+
+	private int sum3(int[] nums) {
+
+//		Given an array of ints length 3, return the sum of all the elements.
+//
+//
+//		sum3([1, 2, 3]) → 6
+//		sum3([5, 11, 2]) → 18
+//		sum3([7, 0, 0]) → 7
+		
+		return nums[0] + nums[1] + nums[2];
 	}
 
 }
